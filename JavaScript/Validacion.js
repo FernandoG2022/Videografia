a77a694df04e65f00e1507d75860a9b8ef2a5a2b
FernandoG2022 <fergiudici@gmail.com>
2022-05-08 00:58:08 -0300

    Commit 07

---
+++ JavaScript/Validacion.js
@@ -0,0 +1,23 @@
+//Validación de formularios VideoIngreso y VideoAsociados con Javascript
+
+function validaEnviarAsociado() {
+    //valido nombre, apellido, mail y password NO NULO
+    if (document.validacion.Nombre.value.length == 0 || 
+        document.validacion.Apellido.value.length == 0 || 
+        document.validacion.Mail.value.length == 0 || 
+        document.validacion.Password.value.length == 0) {
+            alert("Tiene que escribir nombre, apellido, correo electrónico y password")
+            document.validacion.Nombre.focus() //foco en el campo Nombre
+        return 0;
+    }
+}
+
+function validaEnviarIngreso() {
+    //valido email y password NO NULO
+    if (document.validacion.Email.value.length == 0 || 
+        document.validacion.Password.value.length == 0) {
+            alert("Tiene que escribir su email y password")
+            document.validacion.Email.focus() //foco en el campo Email
+        return 0;
+    }
+}
